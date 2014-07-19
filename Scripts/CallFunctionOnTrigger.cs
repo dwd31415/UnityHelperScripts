0a597c2448be167e7fd8fba2fb7071ee180dc7db
Adrian Dawid <adriandwd@gmail.com>
2014-07-19 15:47:07 +0200

    Call function on trigger enter script added
    The call function on trigger enter script does what its name says. It
    can call any function on any gameObject in the scene, if the
    OnTriggerEnter function is called.

---
+++ Scripts/CallFunctionOnTrigger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Copyright (c) 2014 Adrian Dawid
+//This file is part of the collection of usefull unity scripts that is available on github.
+
+//NOTE:
+//To work well this file needs the CallFunctionOnTriggerEnterEditor.cs script that is located in the Editor folder. It has to be 
+//in the Editor folder of your project too.
+
+public class CallFunctionOnTrigger : MonoBehaviour {
+	public string MethodName;
+	public CallObject ObjectToCall;
+	GameObject callingObject;
+	
+	void OnTriggerEnter(Collider other){
+		if (ObjectToCall == CallObject.THIS) {
+			gameObject.SendMessage(MethodName);
+		}
+		if (ObjectToCall == CallObject.ALL_CHILDREN) {
+			for(int index=0;index<transform.childCount;index++)
+			{
+				gameObject.transform.GetChild(index).SendMessage(MethodName);
+			}
+		}
+		if (ObjectToCall == CallObject.ENTERED_OBJECT) {
+			other.gameObject.SendMessage(MethodName);
+		}
+		if (ObjectToCall == CallObject.OTHER) {
+			if(callingObject.activeInHierarchy)
+			{
+				callingObject.SendMessage(MethodName);
+			}
+		}
+	}
+
+	public void SetObject(GameObject newObject)
+	{
+		callingObject = newObject;
+	}
+
+	public GameObject GetObject()
+	{
+		return callingObject;
+	}
+}
+
+public enum CallObject{
+	THIS,
+	ENTERED_OBJECT,
+	ALL_CHILDREN,
+	OTHER
+}
---
+++ Scripts/Editor/CallFunctionOnTriggerEnterEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+//Copyright (c) 2014 Adrian Dawid
+//This file is part of the collection of usefull unity scripts that is available on github.
+//It us useless without the CallFunctionOnTrigger script.
+
+[CustomEditor(typeof(CallFunctionOnTrigger))]
+public class CallFunctionOnTriggerEnterEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+
+		DrawDefaultInspector ();
+
+		CallFunctionOnTrigger tar=target as CallFunctionOnTrigger;
+		if (tar.ObjectToCall == CallObject.OTHER) {
+			tar.SetObject(EditorGUILayout.ObjectField("Other:",tar.GetObject(),typeof(GameObject),true) as GameObject);
+		}
+
+	
+	}
+}
